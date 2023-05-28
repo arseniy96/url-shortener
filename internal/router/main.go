3cbd0ed4f6f1ec97763a9fc118a32ea6d099a0db
arsenii <arsenzhar@gmail.com>
2023-05-28 19:12:39 +0200

    Implemented compression/decompression

--- internal/router/main.go
+++ internal/router/main.go
@@ -2,12 +2,14 @@ package router
 
 import (
 	"github.com/arseniy96/url-shortener/internal/logger"
+	"github.com/arseniy96/url-shortener/internal/middlewares/gzip"
 	"github.com/arseniy96/url-shortener/internal/server"
 	"github.com/go-chi/chi/v5"
 )
 
 func NewRouter(server server.Server) chi.Router {
 	router := chi.NewRouter()
+	router.Use(gzip.Middleware)
 	router.Post("/", logger.RequestWithLog(server.CreateLink))
 	router.Get("/{url_id}", logger.RequestWithLog(server.ResolveLink))
 	router.Post("/api/shorten", logger.RequestWithLog(server.APICreateLink))
